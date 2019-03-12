2db5428fde655f3f774b9d3795f77955a2b8e4b5
Michelle Wedfald <michelle.wedfald.nielsen@gmail.com>
2019-03-12 11:57:38 +0100

    Første opdatering af modellen

--- TheDeptBook/TheDeptBook/Model/DeptModel.cs
+++ TheDeptBook/TheDeptBook/Model/DeptModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,42 @@ using System.Threading.Tasks;
 namespace TheDeptBook.Model
 {
    public class DeptModel
-   {
-      public double Value { get; set; }
+   {    
+        public string Name { get; set; }
+        public double Debit { get; set; }
+        public DateTime Date{ get; set; }
 
+        private List<double> debits;
+        private List<Tuple<string, double, DateTime>> deptors;
+
+        public void AddNewDeptor(string name, double debit)
+        {
+            DateTime date = DateTime.Now;
+            
+            Tuple<string, double, DateTime> deptor = new Tuple<string, double, DateTime>(name, debit,date);
+
+            deptors.Add(deptor);
+        }
+
+        public List<double> GetDebits(string name)
+        { 
+            foreach(var item in deptors)
+            {
+                if (item.Item1 == name)
+                {
+                   debits.Add(item.Item2);
+                }
+            }
+            return debits;
+        }
+        public double CalculateDept(List<string> deptor)
+        {
+            foreach (var name in deptor)
+            {
+               List<double> debits = GetDebits(name);
+             
+            }
+            return debits.Sum();
+        }
    }
 }
