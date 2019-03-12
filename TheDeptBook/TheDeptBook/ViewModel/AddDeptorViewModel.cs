65f9d278c0986f844ea23b596f4b2192d728d4b8
Christiane95 <christiane@noerkjaer-svendsen.dk>
2019-03-12 11:11:57 +0100

    Interfaces tilføjet til viewModels

--- TheDeptBook/TheDeptBook/ViewModel/AddDeptorViewModel.cs
+++ TheDeptBook/TheDeptBook/ViewModel/AddDeptorViewModel.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TheDeptBook.Annotations;
 using TheDeptBook.Model;
 
 namespace TheDeptBook.ViewModel
 {
-   class AddDeptorViewModel
+   class AddDeptorViewModel: INotifyPropertyChanged
    {
       private DeptModel _deptModel;
       public AddDeptorViewModel(DeptModel deptModel)
       {
          _deptModel = deptModel;
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
       }
    }
 }
--- TheDeptBook/TheDeptBook/ViewModel/RegisteredDebitViewModel.cs
+++ TheDeptBook/TheDeptBook/ViewModel/RegisteredDebitViewModel.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using TheDeptBook.Annotations;
 using TheDeptBook.Model;
 
 namespace TheDeptBook.ViewModel
 {
-   class RegisteredDebitViewModel
+   class RegisteredDebitViewModel: INotifyPropertyChanged
    {
       private DeptModel _deptModel;
       public RegisteredDebitViewModel(DeptModel deptModel)
       {
          _deptModel = deptModel;
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
       }
    }
 }
