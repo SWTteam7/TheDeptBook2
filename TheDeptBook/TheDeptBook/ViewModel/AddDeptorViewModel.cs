50a9b8d8bf10a47aa944f44ee1f1ee485be11710
Christiane95 <christiane@noerkjaer-svendsen.dk>
2019-03-14 00:00:43 +0100

    Arbejdet lidt på addDeptorViewModel

--- TheDeptBook/TheDeptBook/ViewModel/AddDeptorViewModel.cs
+++ TheDeptBook/TheDeptBook/ViewModel/AddDeptorViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TheDeptBook.Annotations;
 using TheDeptBook.Model;
 
@@ -30,5 +31,78 @@ namespace TheDeptBook.ViewModel
          _deptModel = deptModel;
          _navigate = nav;
       }
-}
+
+      public string Name
+      {
+         get => _deptModel.Name;
+         set
+         {
+            if (value!=_deptModel.Name)
+            {
+               _deptModel.Name = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      public double InitValue
+      {
+         get => _deptModel.Debit;
+         set
+         {
+            if (value != _deptModel.Debit)
+            {
+               _deptModel.Debit = value;
+               OnPropertyChanged();
+               
+            }
+         }
+      }
+
+      public DateTime time { get; private set; }
+
+
+      private ICommand _saveDeptorCommand;
+
+      public ICommand SaveDeptorCommand
+      {
+         get
+         {
+            if (_saveDeptorCommand == null)
+            {
+               _saveDeptorCommand=new RelayCommand(SaveDeptor);
+            }
+
+            return _saveDeptorCommand;
+         }
+         
+      }
+
+      private void SaveDeptor()
+      {
+         _deptModel.AddNewDeptor(Name,InitValue);
+      }
+
+      private ICommand _closeCommand;
+
+      public ICommand CloseCommand
+      {
+         get
+         {
+            if (_closeCommand == null)
+            {
+               _closeCommand=new RelayCommand(CloseAddDeptor);
+
+            }
+
+            return _closeCommand;
+         }
+      }
+
+      private void CloseAddDeptor()
+      {
+         //_navigate.show();
+      }
+
+   }
 }
